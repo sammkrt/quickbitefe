de90070f6d718ffe3be7396fa3067d41b0e8890c
jacopo-zarri <zarri.jacopo1992@gmail.com>
2023-03-24 12:16:35 +0100

    corrected localhost

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   const fetchingdata = async () => {
     const response = await fetch(
-      "https://localhost:7209/WeatherForecast"
+      "https://localhost:7229/WeatherForecast"
     );
     const result = await response.json();
     console.log(result);
