bb36cc1cb403ea308545b4564b782c64ef478c93
jacopo-zarri <zarri.jacopo1992@gmail.com>
2023-03-24 12:07:43 +0100

    initial create

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+
+  const fetchingdata = async () => {
+    const response = await fetch(
+      "https://localhost:7209/WeatherForecast"
+    );
+    const result = await response.json();
+    console.log(result);
+  };
+  fetchingdata();
+  return <></>;
+}
+export default App;
