242338cd87a584a7082d76f7e8c5d21cd1c8e22f
jacopo-zarri <zarri.jacopo1992@gmail.com>
2023-03-24 14:11:38 +0100

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 function App() {
   const fetchingdata = async () => {
-    const response = await fetch("https://quickbitebe.azurewebsites.net/weatherforecast");
+    // const response = await fetch("https://localhost:7229/WeatherForecast");
+    const response = await fetch("https://quickbitebe.azurewebsites.net/weatherforecast"
+    );
     const result = await response.json();
     console.log(result);
   };
   fetchingdata();
-  return <></>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
 }
 export default App;
