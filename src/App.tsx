c7b24447aa7113b2dabbaf333e6dc734965868c8
jacopo-zarri <zarri.jacopo1992@gmail.com>
2023-03-24 12:47:50 +0100

    clean up testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import React from "react";
-
 function App() {
-
   const fetchingdata = async () => {
-    const response = await fetch(
-      "https://localhost:7229/WeatherForecast"
-    );
+    const response = await fetch("https://localhost:7229/WeatherForecast");
     const result = await response.json();
     console.log(result);
   };
