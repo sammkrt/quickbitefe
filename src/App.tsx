2d7f14b1d782ae736098ce6351c4217c393f2072
jacopo-zarri <zarri.jacopo1992@gmail.com>
2023-03-24 14:07:20 +0100

    add correct url

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   const fetchingdata = async () => {
-    const response = await fetch("https://localhost:7229/WeatherForecast");
+    const response = await fetch("https://quickbitebe.azurewebsites.net/weatherforecast");
     const result = await response.json();
     console.log(result);
   };
