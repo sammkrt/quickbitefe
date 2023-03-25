bb50c8e7e170bcddec66195ea5606b7943b9e077
jacopo-zarri <zarri.jacopo1992@gmail.com>
2023-03-25 17:40:58 +0100

    added new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./Login";
+import Start from "./start/Start";
+import Register from "./register/Register";
+import Login from "./login/Login";
 import "./App.css";
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Start />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
+    </Routes>
   );
 }
 export default App;
---
+++ src/register/Register.tsx
@@ -0,0 +1,31 @@
+import "./Register.css";
+function Register() {
+  return (
+    <main className="register-main">
+      <h1 className="register-h1">Quickbite</h1>
+      <img className="register-img" src="./assets/logo.png" />
+      <section className="register-section">
+        <h2 className="register-h2">Register</h2>
+        <form>
+          <p className="register-p">Name</p>
+          <input className="register-input" placeholder="Name" type="text" />
+          <br />
+          <p className="register-p">Email</p>
+          <input className="register-input" placeholder="Email" type="text" />
+          <br />
+          <p className="register-p">Password</p>
+          <input
+            className="register-input"
+            placeholder="Password"
+            type="text"
+          />
+          <br />
+          <button className="register-button" type="submit">
+            Register
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+}
+export default Register;
