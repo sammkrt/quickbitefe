f01398fc73f6ec7e6ea1b7d57eb74a139cda4220
LieC4 <danielmorillascarpio@gmail.com>
2023-03-28 15:46:28 +0200

    fetching userdata

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,33 @@ import Payment from "./pages/Payment/Payment";
 import Restaurant from "./pages/Restaurants/Restaurants";
 import MyOrder from "./pages/MyOrder/MyOrder";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+interface User {
+  firstName : string,
+  lastName : string,
+  email : string,
+  address: string,
+  phoneNumber : string,
+}
+
+
 function App() {
+
+const [user, setUser] = useState('')
+const fetchUser = async()=> {
+const result = await fetch("http://localhost:5242/Auth/user");
+const data = await result.json();
+setUser(data);
+console.log(data)
+};
+useEffect(()=> {
+  fetchUser();
+}, []);
+
+
+
+
   return (
     <Routes>
       <Route path="/" element={<StartPage />} />
