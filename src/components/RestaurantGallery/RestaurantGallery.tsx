e97dbc32fb57c7c8495cf69d856d261bc5b22ec1
jacopo-zarri <zarri.jacopo1992@gmail.com>
2023-04-03 10:39:21 +0200

    clean up

--- src/components/RestaurantGallery/RestaurantGallery.tsx
+++ src/components/RestaurantGallery/RestaurantGallery.tsx
@@ -1,15 +1,12 @@
 import { Link } from "react-router-dom";
-import { RestaurantModel } from "../../types/Types";
+import { Props } from "../../types/Types";
 import "./RestaurantGallery.css";
-interface Props {
-  restaurant: RestaurantModel[];
-}
 const RestaurantGallery: React.FC<Props> = ({ restaurant }) => {
   return (
     <main>
       {restaurant.map((restaurant) => (
         <Link to={`/${restaurant.id}`}>
-          <figure className="restaurantgallery-figure" key={restaurant.id}>
+          <figure className="restaurantgallery-figure">
             <div className="restaurantgallery-container">
               <img
                 className="restaurantgallery-img"
